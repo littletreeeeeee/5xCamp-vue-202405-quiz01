0f87bfc17e3f1ae4db44955e0e8e9b1830d70379
Kuro Hsu <kurotanshi@gmail.com>
2024-05-19 21:36:02 +0800

    新增題目

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,69 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+const uBikeStops = ref([]);
+
+// 欄位說明:
+// sno：站點代號、 sna：場站名稱(中文)、 total：場站總停車格、
+// available_rent_bikes：場站目前可用車輛數量、 
+// sarea：場站區域(中文)、 mday：資料更新時間、
+// lat：緯度、 lng：經度、 ar：地(中文)、 sareaen：場站區域(英文)、
+// snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
+fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
+  .then(res => res.text())
+  .then(data => {
+    uBikeStops.value = JSON.parse(data);
+  });
+
+const timeFormat = (val) => {
+  // 時間格式
+  const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
+  return val.replace(pattern, '$1/$2/$3 $4:$5:$6');
+};
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<template>  
+<!--
+練習： YouBike 臺北市公共自行車即時資訊 
+完成下列表格，並加入: 1. 站點名稱搜尋、2. 目前可用車輛 / 總停車格 的排序功能
+-->
+  <div id="app">
+    <p style="padding-left: 1rem;">
+      站點名稱搜尋: <input type="text">
+    </p>
+    
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>場站名稱</th>
+          <th>場站區域</th>
+          <th>目前可用車輛
+            <i class="fa fa-sort-asc" aria-hidden="true"></i>
+            <i class="fa fa-sort-desc" aria-hidden="true"></i>
+          </th>
+          <th>總停車格
+            <i class="fa fa-sort-asc" aria-hidden="true"></i>
+            <i class="fa fa-sort-desc" aria-hidden="true"></i>
+          </th>
+          <th>資料更新時間</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(s, idx) in uBikeStops" :key="s.sno">
+          <td>{{ idx +1 }}</td>
+          <td>{{ s.sna }}</td>
+          <td>{{ s.sarea }}</td>
+          <td>{{ s.available_rent_bikes }}</td>
+          <td>{{ s.total }}</td>
+          <td>{{ timeFormat(s.mday) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#app {
+  margin-top: 1rem;
 }
 </style>
